42c79f8be93ce1caa1902b5a7e0d2bb7d1f3d946
Kaoutar Qanouni <kaoutar.qanouni@gmail.com>
2025-07-20 15:00:14 +0000

    adding string function return self.name to models.py

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -17,4 +17,6 @@ class Librarian(models.Model):
     name = models.CharField(max_length=100)
     library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name='librarian')    
     
+def __str__(self):
+    return self.name
 
