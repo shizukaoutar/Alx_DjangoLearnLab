31775b1ef6b53df54e60de76afe2cac27ffacd13
Kaoutar Qanouni <kaoutar.qanouni@gmail.com>
2025-07-20 15:05:04 +0000

    updating query samples commands

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,8 +1,8 @@
 books_by_author = Book.objects.filter(author__name='John Doe')
 print(books_by_author)
 
-books = Book.objects.all()
-print(books)
+books = Book.objects.filter(libraries__name='Library 1')
+print(books.all())
 
 
 librarian = Librarian.objects.filter(library__name='Library 1')
