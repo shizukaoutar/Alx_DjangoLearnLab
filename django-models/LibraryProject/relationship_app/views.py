1ff2379d53ce205751d09d1b7135e151b3a6969b
Kaoutar Qanouni <kaoutar.qanouni@gmail.com>
2025-07-20 18:05:18 +0000

    imports

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import DetailView
-from .models import Book, Library
+from .models import Library, Book
 
 # Create your views here.
 def book_list(request):
