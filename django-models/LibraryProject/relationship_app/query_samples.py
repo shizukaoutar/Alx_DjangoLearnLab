25bdd2f344e4c42437fa54b12dc18dc62d7f67ee
Kaoutar Qanouni <kaoutar.qanouni@gmail.com>
2025-07-20 15:07:11 +0000

    updating query commands from filter to get

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,9 +1,9 @@
 books_by_author = Book.objects.filter(author__name='John Doe')
 print(books_by_author)
 
-books = Book.objects.filter(libraries__name='Library 1')
-print(books.all())
+books = Book.objects.get(libraries__name='Library 1')
+print(books)
 
 
-librarian = Librarian.objects.filter(library__name='Library 1')
+librarian = Librarian.objects.get(library__name='Library 1')
 print(librarian)
