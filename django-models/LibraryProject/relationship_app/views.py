578e04ca9ad5d2347a23f3cf219114a6e2cc4c0a
Kaoutar Qanouni <kaoutar.qanouni@gmail.com>
2025-07-20 18:03:06 +0000

    updating urls in views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,14 +6,14 @@ from .models import Book, Library
 def book_list(request):
     books = Book.objects.all()
     context = {'books': books}
-    return render(request, 'list_books.html', context)
+    return render(request, 'relationship_app/list_books.html', context)
 
 
 
 
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'library_detail.html'
+    template_name = 'relationship_app/library_detail.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
