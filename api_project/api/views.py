4b75e3fb6bf1e63786094ed9b0067f79dd589675
Kaoutar Qanouni <kaoutar.qanouni@gmail.com>
2025-08-03 01:58:15 +0000

    Implementing CRUD operations with viewsets and router in DRF

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,7 +1,13 @@
 from .models import Book
 from .serializers import BookSerializer
 
+
 # Create your views here.
 class BookList(rest_framework.generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class BookViewSet(rest_framework.viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
