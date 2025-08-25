6e6c0616114bb696f7c606802d75251acfe15b66
Kaoutar Qanouni <kaoutar.qanouni@gmail.com>
2025-08-25 18:31:47 +0000

    Implementing filtering, searching and ordering in DRF

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.pagination import PageNumberPagination
-from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import rest_framework
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
