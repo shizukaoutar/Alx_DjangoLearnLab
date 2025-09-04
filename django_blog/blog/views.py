064b6221109b452b62721254adf6dfa33dbddd1c
Kaoutar Qanouni <kaoutar.qanouni@gmail.com>
2025-09-04 20:33:13 +0000

    Implementing the blog's user authentication system

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CustomRegistrationForm, UserProfileForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+from django.contrib.auth.forms import AuthenticationForm
+
 
 # Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        form = CustomRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = CustomRegistrationForm()
+    return render(request, 'registration/register.html', {'form': form})
+
+
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=request.user.profile)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
+    else:
+        form = UserProfileForm(instance=request.user.profile)
+    return render(request, 'registration/profile.html', {'form': form})
+
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'registration/login.html', {'form': form})
+
+
+def logout(request):
+    logout(request)
+    return redirect('login')
