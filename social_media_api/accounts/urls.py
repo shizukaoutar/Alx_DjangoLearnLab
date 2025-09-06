5407a487a47b8fc2f84e46997bf4423cabb8df62
Kaoutar Qanouni <kaoutar.qanouni@gmail.com>
2025-09-06 20:47:24 +0000

    adjusting accounts.urls

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -4,6 +4,6 @@ from .views import RegisterView, LoginView, FollowUserView, UnfollowUserView
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
-    path('follow-user/<int:user_id>/', FollowUserView.as_view(), name='follow-user'),
-    path('unfollow-user/<int:user_id>/', UnfollowUserView.as_view(), name='unfollow-user'),
+    path('follow/<int:user_id>/', FollowUserView.as_view(), name='follow-user'),
+    path('unfollow/<int:user_id>/', UnfollowUserView.as_view(), name='unfollow-user'),
 ]
